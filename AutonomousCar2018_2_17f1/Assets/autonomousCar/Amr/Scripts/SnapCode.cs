8a6d7e3ee3103f263a950c8ac55e228701f746ec
Loai Alaa <loai.alaa7@gmail.com>
2019-03-11 22:35:29 +0200

    socket code in SnapCode.cs

--- AutonomousCar2018_2_17f1/Assets/autonomousCar/Amr/Scripts/SnapCode.cs
+++ AutonomousCar2018_2_17f1/Assets/autonomousCar/Amr/Scripts/SnapCode.cs
@@ -1,15 +1,102 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 using UnityEngine;
+using System.IO;
+using System;
+
+	
 
 [RequireComponent(typeof(Camera))]
-public class Snapshots : MonoBehaviour
+public class SnapCode : MonoBehaviour
 {
 
     Camera Snapcam;
-    public int resWidth = 256;
-    public int resHight = 256;
+    public int resWidth = 1080;
+    public int resHight = 1080;
+    int snapCounter;
+    int frameCounter = 0;
+    public int snapFrameCounter = 50;
+    int launchCount;
+    private List<string[]> rowData = new List<string[]>();
+    int counting;
+		#region private members 	
+	private TcpClient socketConnection; 	
+	private Thread clientReceiveThread; 	
+	#endregion  	
+    private void Start()
+    {   ConnectToTcpServer();
+        snapCounter = 0;
+        counting=0;
+    }
+//---------------------------------------------------------------------------------------------------------------
+	void Update () {         
+		if (Input.GetKeyDown(KeyCode.Space)) {             
+			SendMessage();         
+		}     
+	}  	
 
+	    private void ConnectToTcpServer () { 		
+		try {  			
+			clientReceiveThread = new Thread (new ThreadStart(ListenForData)); 			
+			clientReceiveThread.IsBackground = true; 			
+			clientReceiveThread.Start();  		
+		} 		
+		catch (Exception e) { 			
+			Debug.Log("On client connect exception " + e); 		
+		} 	
+	}
+        private void ListenForData() { 		
+		try { 			
+			socketConnection = new TcpClient("127.0.0.1", 1234);  			
+			Byte[] bytes = new Byte[1024];             
+			while (true) { 				
+				// Get a stream object for reading 				
+				using (NetworkStream stream = socketConnection.GetStream()) { 					
+					int length; 					
+					// Read incomming stream into byte arrary. 					
+					while ((length = stream.Read(bytes, 0, bytes.Length)) != 0) { 						
+						var incommingData = new byte[length]; 						
+						Array.Copy(bytes, 0, incommingData, 0, length); 						
+						// Convert byte array to string message. 						
+						string serverMessage = Encoding.ASCII.GetString(incommingData); 						
+						Debug.Log("server message received as: " + serverMessage); 					
+					} 				
+				} 			
+			}         
+		}         
+		catch (SocketException socketException) {             
+			Debug.Log("Socket exception: " + socketException);         
+		}     
+	}  	
+
+
+    private void SendMessage() {         
+		if (socketConnection == null) {             
+			return;         
+		}  		
+		try { 			
+			// Get a stream object for writing. 			
+			NetworkStream stream = socketConnection.GetStream(); 			
+            
+			if (stream.CanWrite) {                 
+	    
+			Stream imageFileStream = File.OpenRead("L:\\Bachelor\\CarSimulation\\AutonomousCar2018_2_17f1\\Assets\\Snapshots\\butterfly.jpg");
+            byte[] clientMessageAsByteArray=new byte[imageFileStream.Length];	// Convert string message to byte array.                 
+		    imageFileStream.Read(clientMessageAsByteArray, 0, (int)imageFileStream.Length);
+            //	byte[] clientMessageAsByteArray = Encoding.ASCII.GetBytes(clientMessage); 				
+				// Write byte array to socketConnection stream.                 
+				stream.Write(clientMessageAsByteArray, 0, clientMessageAsByteArray.GetLength(0));                 
+				Debug.Log("Client sent his message - should be received by server");             
+			}         
+		} 		
+		catch (SocketException socketException) {             
+			Debug.Log("Socket exception: " + socketException);         
+		}     
+	} 
+	//------------------------------------------------------------------------------------------------
     private void Awake()
     {
         Snapcam = GetComponent<Camera>();
@@ -17,26 +104,92 @@ public class Snapshots : MonoBehaviour
         {
             Snapcam.targetTexture = new RenderTexture(resWidth, resHight, 24);
         }
+        launchCount = PlayerPrefs.GetInt("TimesLaunched", 0);
+
+        // After Grabbing 'TimesLaunched' we increment the value by 1
+        launchCount = launchCount + 1;
+
+        // Set 'TimesLaunched' To The Incremented Value
+        PlayerPrefs.SetInt("TimesLaunched", launchCount);
+
+        // Now I Would Destroy The Script Or Whatever You
+        // Want To Do To Prevent It From Running Multiple
+        // Times In One Launch Session
     }
+
     // Update is called once per frame
 
     void LateUpdate()
     {
+            if (frameCounter >= snapFrameCounter)
+        {
         Texture2D snapshot = new Texture2D(resWidth, resHight, TextureFormat.RGB24, false);
         Snapcam.Render();
         RenderTexture.active = Snapcam.targetTexture;
         snapshot.ReadPixels(new Rect(0, 0, resWidth, resHight), 0, 0);
         byte[] bytes = snapshot.EncodeToPNG();
         string fileName = SnapshotName();
+        snapCounter++;
         System.IO.File.WriteAllBytes(fileName, bytes);
+		Save(snapCounter,launchCount);
         Debug.Log("Snapshot taken !");
+        }
+        else{
+            frameCounter++;
+        }
 
     }
 
     string SnapshotName()
     {
-        return string.Format("{0}/Snapshots/snap_{1}.png",
+   return string.Format("{0}/Snapshots/Session_{1}_{2}.png",
             Application.dataPath,
-            System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+           launchCount,
+           snapCounter);
+    }
+
+//---------------------------------------------------------------------------------------------------------------------------
+
+	 void Save(int count,int session){
+
+        // Creating First row of titles manually..
+
+        rowData = new List<string[]>();
+        string[] rowDataTemp = new string[2];
+
+        if(counting == 0){
+        Debug.Log(counting);
+        rowDataTemp[0] = "ID";
+        rowDataTemp[1] = "Session";
+        rowData.Add(rowDataTemp);
+        counting++;
+        }
+        
+        // You can add up the values in as many cells as you want.
+            rowDataTemp = new string[2];
+            rowDataTemp[0] = snapCounter+""; // ID
+            rowDataTemp[1] = session+""; // ID
+            rowData.Add(rowDataTemp);
+
+        string[][] output = new string[rowData.Count][];
+
+        for(int i = 0; i < output.Length; i++){
+            output[i] = rowData[i];
+        }
+        Debug.Log(output);
+        int     length         = output.GetLength(0);
+        string  delimiter     = ",";
+
+        StringBuilder sb = new StringBuilder();
+        
+        for (int index = 0; index < length; index++)
+            sb.AppendLine(string.Join(delimiter, output[index]));
+        
+        
+        string filePath = Application.dataPath +"/CSV/"+"Saved_data.csv";
+
+        StreamWriter outStream = System.IO.File.AppendText(filePath);
+        outStream.WriteLine(sb);
+        outStream.Close();
     }
 }
