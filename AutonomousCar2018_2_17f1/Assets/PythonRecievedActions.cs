ab5b39cb0b8a10435ec696f2041cc2cd98a78dc5
Loay Naser <loai.alaa7@gmail.com>
2019-06-17 05:19:34 +0200

    clean code

--- AutonomousCar2018_2_17f1/Assets/PythonRecievedActions.cs
+++ AutonomousCar2018_2_17f1/Assets/PythonRecievedActions.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿#Created by: Loay Naser, Mohamed Lotfy
+
+
+using System.Collections.Generic;
 using UnityEngine;
 using System.Net;
 using System.Net.Sockets;
@@ -12,13 +15,12 @@ public class PythonRecievedActions : MonoBehaviour
 {
 	public string IP = "127.0.0.1"; 
 	public int Port = 1234;
-	//public byte[] dane;
 	public Socket client;
 	int WaitForCarToLand=20;
 
 	public CarController m_Car; // the car controller we want to use
 
-	public void Changing()
+	public void getCommands()
 	{
 		client = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 		client.Connect (IP, Port);//connecting port with ip address 
@@ -28,17 +30,13 @@ public class PythonRecievedActions : MonoBehaviour
 		string szReceived = System.Text.Encoding.ASCII.GetString(b, 0, k);//coming data is in bytes converting into string 
 
 		if (client.Connected)
-		{   //Debug.Log(szReceived);
+		{   
 			string[] words = szReceived.Split(' ');//split data into string data is in 2.2 3.3 4.0
-			Debug.Log(words[0]);
-			// which direction will we move the car in, f--> foreward , b--> backward , r--> right , l--> left
-			Debug.Log(m_Car.CurrentSpeed);
 			switch (words[0]) 
       		{
 				case "2":
 				    Debug.Log("right");
 					 if(m_Car.CurrentSpeed > 20.0){
-				     Debug.Log("brake right");
                      m_Car.Move(1.0f, 0f, 0f, 0f);
                      }else{
 					 m_Car.Move(1.0f, 0.5f, 0.5f, 0f);
@@ -48,16 +46,13 @@ public class PythonRecievedActions : MonoBehaviour
 					m_Car.Move(0f, 0f, 0f, 0f);
 					break;
 				case "1":
-			    	Debug.Log("left");
 				    if(m_Car.CurrentSpeed > 20.0){
-					    Debug.Log("brake left");
                        	m_Car.Move(-1.0f, 0f, 0f, 0f);
                      }else{
 					   m_Car.Move(-1.0f, 0.5f, 0.5f, 0f);
 					 }
 					 break;
 				case "0":
-				    Debug.Log("brake");
 					if(m_Car.CurrentSpeed < 20.0){
                      m_Car.Move(0f, 1.0f, 1.0f, 0f);
                      }else{
@@ -66,7 +61,6 @@ public class PythonRecievedActions : MonoBehaviour
 					
 					break;
 				case "3":{
-				    Debug.Log("forward");
 					m_Car.Move(0f, 1.0f, 1.0f, 0f);
 					break;
 				}
@@ -78,15 +72,11 @@ public class PythonRecievedActions : MonoBehaviour
 		}
 		client.Close();	
 	}
-	void Start ( )
-	{
-		//m_Car = GetComponent<CarController>();
-	}
 
 	void Update ()
 	{
 		if(WaitForCarToLand-- <=0) // wait for the car to land on the track from the sky before moving
-			Changing ();
+			getCommands();
 	}
 }
 }
--- AutonomousCar2018_2_17f1/Assets/autonomousCar/Amr/Scripts/MapGen/snapshotHelper.cs
+++ AutonomousCar2018_2_17f1/Assets/autonomousCar/Amr/Scripts/MapGen/snapshotHelper.cs
@@ -15,9 +15,6 @@ public class snapshotHelper : MonoBehaviour {
                  string keyName=vKey+"";
                  if(keyName.CompareTo("UpArrow")==0 || keyName.CompareTo("DownArrow")==0 || keyName.CompareTo("LeftArrow")==0 || keyName.CompareTo("RightArrow")==0)
                  {
-                    // snapCam.setcurrentKey(vKey+"");
-                    // snapCam.takeSnapshot();
-
                     obsCam.setcurrentKey(vKey+"");
                     obsCam.takeSnapshot();
                  } 
